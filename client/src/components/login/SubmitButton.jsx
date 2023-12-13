e85c1cd1347f24df14a9a0814a840ba5de95545d
YoonJae Kim <yoonjaekim3@gmail.com>
2023-12-13 14:29:13 -0600

    Fixed recaptcha so it only shows on signup

--- client/src/components/login/SubmitButton.jsx
+++ client/src/components/login/SubmitButton.jsx
@@ -13,9 +13,8 @@ const SubmitButton = ({ isLogin }) => {
     return (
         <div className="mt-5">
             <button className="w-full bg-purple-500 border-2 border-purple-500 hover:bg-purple-600 py-3 text-center text-white rounded">{buttonText}</button>
-            {isLogin ? (<></>) : 
-            (<PolicyModal />)}
-            <ReCAPTCHA sitekey={siteKey} onChange={onChange} />,
+            {isLogin ? (<></>) : (<><PolicyModal /><ReCAPTCHA sitekey={siteKey} onChange={onChange} /></>)}
+
         </div>
     )
 }
