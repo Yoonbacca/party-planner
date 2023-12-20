f036c75728a0f661457903a9ab02192417d0651b
YoonJae Kim <yoonjaekim3@gmail.com>
2023-12-20 13:54:33 -0600

    Consolidated seed docs into one file

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,7 +1,7 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Party } = require('../models');
 const cleanDB = require('./cleanDB');
-const userSeeds = require('./userSeeds.json');
+const bcrypt = require('bcrypt');
 
 // Once method will wait for the connection to be established before executing the callback function
 db.once('open', async () => {
@@ -9,8 +9,65 @@ db.once('open', async () => {
         console.log('server/utils/seed.js: db.once()');
         console.log('seed: Running cleanDB...');
         await cleanDB('User', 'users');
+        await cleanDB('Party', 'parties');
+
+        const hashedPassword = await bcrypt.hash("password", 10);
+
         console.log('seed: Creating users...');
-        await User.create(userSeeds);
+        const users = await User.insertMany([
+            {
+                username: "Timtom",
+                email: "timtom@tim.tom",
+                password: hashedPassword
+            },
+            {
+                username: "Lernantino",
+                email: "lernantino@aol.com",
+                password: hashedPassword  
+            },
+            {
+                username: "Amiko",
+                email: "amiko@gmail.com",
+                password: hashedPassword
+            }
+        ]);
+        console.log('seed: Users created...');
+
+        console.log('seed: Creating parties...');
+        const parties = await Party.insertMany([
+            {
+                name: "Tim's Party",
+                description: "A party for Tim",
+                location: "Tim's House",
+                host: users[0]._id,
+                guests: [users[1]._id, users[2]._id]
+            },
+            {
+                name: "Lernantino's Party",
+                description: "A party for Lernantino",
+                location: "Lernantino's House",
+                host: users[1]._id,
+                guests: [users[0]._id, users[2]._id]
+            }
+        ]);
+        console.log('seed: Parties created...');
+
+        users.map(async (user) => {
+            console.log("userid",user._id)
+            await User.findOneAndUpdate(
+                { 
+                    _id: user._id
+                },
+                { 
+                    $push: { 
+                        parties: parties.filter((party) => 
+                            party.host.includes(user._id)
+                        ),
+                    }, 
+                },
+            );
+        });
+
 
         console.log('seed: Done!');
         process.exit(0);
