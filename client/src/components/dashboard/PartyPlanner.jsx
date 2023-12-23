a4f81ea9acbee1a7398428f094d481c28b8118dd
YoonJae Kim <yoonjaekim3@gmail.com>
2023-12-22 21:20:22 -0600

    Added antd to modules

--- client/src/components/dashboard/PartyPlanner.jsx
+++ client/src/components/dashboard/PartyPlanner.jsx
@@ -3,13 +3,10 @@ import { useMutation } from '@apollo/client';
 import { GET_USER } from '../../utils/queries';
 import { ADD_PARTY } from '../../utils/mutations'
 import { Datepicker } from 'flowbite-react';
-import TimePicker from 'react-time-picker'
-import 'react-time-picker/dist/TimePicker.css';
-import 'react-clock/dist/Clock.css';
+import { TimePicker } from 'antd';
 
 const PartyPlanner = ({ setPartyPlanning }) => {
     const [formData, setFormData] = useState({name: '', description: '', date: '', time: '', location: '', guestList: ''});
-    const [value, onChange] = useState('10:00');
 
     const [addParty, {error}] = useMutation(ADD_PARTY);
   
@@ -56,9 +53,16 @@ const PartyPlanner = ({ setPartyPlanning }) => {
               </div>              
               <div className="mt-5 flex flex-col">
                 <h2 className="text-2xl mb-1">Date and Time</h2>
-                <Datepicker id="datepicker" name="date" minDate={new Date(Date.now())} onSelectedDateChanged={handleDateChange} />
+                <div className="flex flex-row">
+                  <Datepicker id="datepicker" name="date" minDate={new Date(Date.now())} onSelectedDateChanged={handleDateChange} />
+                  <div className="relative max-w-sm">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                    </div>
+                    
+                  </div>
+                </div>
               </div>
-                <TimePicker name="time" onChange={handleInputChange} />
+              
             </form>
         </div>
     </>
@@ -66,3 +70,13 @@ const PartyPlanner = ({ setPartyPlanning }) => {
 }
 
 export default PartyPlanner;
+
+
+{/* <div class="relative max-w-sm">
+  <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+    </svg>
+  </div>
+  <input datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+</div> */}
