12c1dd958109ddf8a5ab9ffe165d7981815a4966
YoonJae Kim <yoonjaekim3@gmail.com>
2023-12-27 16:08:30 -0600

    Removed scratch form for prefab form from antd

--- client/src/components/dashboard/PartyPlanner.jsx
+++ client/src/components/dashboard/PartyPlanner.jsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { GET_USER } from '../../utils/queries';
 import { ADD_PARTY } from '../../utils/mutations'
-import { DatePicker, TimePicker, Space } from 'antd';
+import { DatePicker, TimePicker, Space, Form, Input, Checkbox, Button } from 'antd';
 import dayjs from 'dayjs';
 
 const PartyPlanner = ({ setPartyPlanning }) => {
     const [formData, setFormData] = useState({name: '', description: '', date: '', time: '', location: '', guestList: ''});
-    const format = 'HH:mm';
+    const format = 'HH:mm A';
     const [addParty, {error}] = useMutation(ADD_PARTY);
   
+    const onFinish = (values) => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
+
     const handleInputChange = (event) => {
       const { name, value } = event.target;
       setFormData({ ...formData, [name]: value });
@@ -44,22 +51,62 @@ const PartyPlanner = ({ setPartyPlanning }) => {
     <>
         <div className="flex flex-col mx-auto p-4 border bg-white border-neutral-300 rounded-lg shadow sm:p-8 ">
         <h2 className="text-5xl mb-4">Party Planner</h2>
+        <Form
+    name="basic"
+    layout={"vertical"}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Name"
+      name="name"
+      rules={[
+        {
+          required: true,
+          message: "Please input your party's name!",
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      label="Date and Time"
+      name="dateTime"
+
+    >
+      <Space direction="horizontal">
+        <DatePicker size={"large"} onChange={onChange} />  
+        <TimePicker size={"large"} id="timepicker" name="time" minuteStep={15} defaultValue={dayjs('12:08', format)} format={format} use12Hours={true} />
+      </Space>
+    </Form.Item>
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
             <form onSubmit={handleFormSubmit}>
-              <div className="mt-5 flex flex-col">
-                <h2 className="text-2xl mb-1">Name</h2>
-                <input className="flex-1 mb-5 text-base border text-black sm:text-lg" type="text" name="name" placeholder="Name" onChange={handleInputChange} />
-              </div>
-              <div className="mt-5 flex flex-col">
-                <h2 className="text-2xl mb-1">Description</h2>
-                <input className="flex-1 mb-5 text-base border text-black sm:text-lg" type="text" name="description" placeholder="Description" onChange={handleInputChange} />
-              </div>              
+              <Input />
+              <Input />             
               <div className="mt-5 flex flex-col">
                 <h2 className="text-2xl mb-1">Date and Time</h2>
                 <div className="flex flex-row">
-                <Space direction="horizontal">
-                  <DatePicker onChange={onChange} />  
-                  <TimePicker id="timepicker" name="time" minuteStep={15} defaultValue={dayjs('12:08', format)} format={format} use12Hours={true} />
-                </Space>
+
                 </div>
               </div>
               
