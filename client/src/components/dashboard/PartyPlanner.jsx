dca01879480fe4097c6a53ab7958188778d2805a
YoonJae Kim <yoonjaekim3@gmail.com>
2023-12-27 16:32:30 -0600

    Set calendar and time defaults

--- client/src/components/dashboard/PartyPlanner.jsx
+++ client/src/components/dashboard/PartyPlanner.jsx
@@ -25,11 +25,10 @@ const PartyPlanner = ({ setPartyPlanning }) => {
     const onChange = (date, dateString) => {
       console.log(date, dateString);
     };
-    // const handleDateChange = (event) => {
-    //   const selectedDate = new Date(event.toString()).toLocaleDateString('en-US');
-    //   setFormData({ ...formData, date: selectedDate });
-    //   console.log(formData)
-    // };
+    const disabledDate = (current) => {
+      // Can not select days before today and today
+      return current && current < dayjs().endOf('day');
+    };
 
     const handleFormSubmit = async (event) => {
       event.preventDefault();
@@ -50,16 +49,16 @@ const PartyPlanner = ({ setPartyPlanning }) => {
     return (
     <>
         <div className="flex flex-col mx-auto p-4 border bg-white border-neutral-300 rounded-lg shadow sm:p-8 ">
-        <h2 className="text-5xl mb-4">Party Planner</h2>
+        <h2 className="text-4xl my-2">Party Planner</h2>
         <Form
-    name="basic"
-    layout={"vertical"}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
+        name="basic"
+        layout={"vertical"}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        size="large"
+      >
     <Form.Item
-      label="Name"
       name="name"
       rules={[
         {
@@ -68,62 +67,41 @@ const PartyPlanner = ({ setPartyPlanning }) => {
         },
       ]}
     >
+      <h2 className="text-xl my-2">Name</h2>
       <Input />
     </Form.Item>
-
     <Form.Item
-      label="Password"
-      name="password"
+      name="description"
       rules={[
         {
-          required: true,
-          message: 'Please input your password!',
+          required: false,
+          message: 'Please input your description!',
         },
       ]}
     >
-      <Input.Password />
+      <h2 className="text-xl my-2">Description</h2>
+      
+      <Input.TextArea />
     </Form.Item>
+    <h2 className="text-xl my-2">Date and Time</h2>
 
-    <Form.Item
-      label="Date and Time"
-      name="dateTime"
-
-    >
-      <Space direction="horizontal">
-        <DatePicker size={"large"} onChange={onChange} />  
-        <TimePicker size={"large"} id="timepicker" name="time" minuteStep={15} defaultValue={dayjs('12:08', format)} format={format} use12Hours={true} />
-      </Space>
+    <Space direction="horizontal">
+    <Form.Item name="date">
+        <DatePicker size={"large"} defaultValue={dayjs().add(1, 'day')}  disabledDate={disabledDate} onChange={onChange} />  
     </Form.Item>
+    <Form.Item name="time">
+        <TimePicker size={"large"} id="timepicker" name="time" minuteStep={15} defaultValue={dayjs('12:00 AM', format)} format={format} use12Hours={true} />
+    </Form.Item>
+    </Space>
     <Form.Item>
       <Button type="primary" htmlType="submit">
         Submit
       </Button>
     </Form.Item>
   </Form>
-            <form onSubmit={handleFormSubmit}>
-              <Input />
-              <Input />             
-              <div className="mt-5 flex flex-col">
-                <h2 className="text-2xl mb-1">Date and Time</h2>
-                <div className="flex flex-row">
-
-                </div>
-              </div>
-              
-            </form>
         </div>
     </>
     )
 }
 
 export default PartyPlanner;
-
-
-{/* <div class="relative max-w-sm">
-  <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-    </svg>
-  </div>
-  <input datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
-</div> */}
